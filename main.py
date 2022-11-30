e50374ff79a8fa90d4bc9c424c3a8235b95e4070
esreferdal <116751845+esreferdal@users.noreply.github.com>
2022-11-30 16:15:05 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+/******************************************************************************
+Eşref Erdal 02195076034 odev1 basit bir repo olusturma
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
